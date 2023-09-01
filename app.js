96705a2c6f7b98c440082f0a83f63e9b8928ac9e
takashi-kozai <129640872+takashi-kozai@users.noreply.github.com>
2023-09-01 10:25:51 +0900

    ローカルのデータベース接続を行うように追加

--- app.js
+++ app.js
@@ -1,7 +1,28 @@
 const express = require('express');
+const mysql = require('mysql2');
+
 const app = express();
 
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'test',
+    password: 'testtest',
+    database: 'blog'
+});
+
 app.get('/', (req, res) => {
+    console.log("app.get() in");
+    connection.query(
+        'SELECT * FROM articles',
+        (error, results) =>{
+            if(error){
+                console.log(`error: ${error}`);
+            }
+            console.log("database access succeed.");
+            console.dir(results);
+        }
+    );
+
     res.render('hello.ejs');
 });
 
